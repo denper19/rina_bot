b069d4d39acbd1be2af2da60b0aaa72cb2e8a684
Jermito <jermito24@gmail.com>
2023-12-08 18:08:43 +0530

    improving gazebo perf

--- robot/launch/gazebo.launch.py
+++ robot/launch/gazebo.launch.py
@@ -17,6 +17,8 @@ def generate_launch_description():
 
     package_name='robot' #<--- CHANGE ME
 
+    world = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_world.sdf')
+
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
@@ -24,11 +26,18 @@ def generate_launch_description():
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'world': '/home/jermito/rina_ws/src/rina_bot/robot/config/gazebo_world.sdf'}.items()
-    )
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+             )
+
+    # gazebo = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource(
+    #                 os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzserver.launch.py')),
+    #                 launch_arguments={'world': '/home/jermito/rina_ws/src/rina_bot/robot/config/gazebo_world.world'}.items())
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
@@ -39,13 +48,13 @@ def generate_launch_description():
 
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_cont"],
     )
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_broad"],
     )
 
